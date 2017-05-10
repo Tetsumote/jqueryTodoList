8fcf20fcc90e0e8e4352efc695abc7fad8f1d2be
Tetsumote <unionproject88@gmail.com>
2017-05-10 04:49:11 +0200

    com.7 end of project

--- js/script.js
+++ js/script.js
@@ -23,7 +23,36 @@ $(function(){
 		$newItemForm.fadeIn(1000);
 	});
 	
+	//adding element to the list
+	$newItemForm.on('submit',function(e){
+		e.preventDefault();
+		var text = $('input:text').val();
+		$list.append('<li class=\"hot\">' + text + '</li>');
+		$('input:text').val('');
+		updateCount();
+	});
 	
+	//deleting file
+	$list.on('click','li',function(){
+		var $this = $(this);
+		var complete = $this.hasClass('complete');
+		
+		if(complete === true){
+			$this.animate({
+				opacity:0.0,
+				paddingLeft: '+=180'
+			},500,'swing',function(){
+				$this.remove();
+			});
+		}else{
+			item = $this.text();
+			$this.remove();
+			$list
+			.append('<li class=\"complete\">' + item + '</li>')
+			.hide().fadeIn(1000);
+		updateCount();
+		}
+	});
 	
 	
 	
