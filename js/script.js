c4f50926b19c90114f0759f88bac1f9ce422a187
Tetsumote <unionproject88@gmail.com>
2017-05-10 03:55:01 +0200

    com.6 add functionality to the button

--- js/script.js
+++ js/script.js
@@ -4,9 +4,23 @@ $(function(){
 	$list = $('ul');
 	$newItemForm = $('#newItemForm');
 	$newItemButton = $('#newItemButton');
-	
+	//fade in animation
 	$('li').hide().each(function(index){
 		$(this).delay(450 * index).fadeIn(1600);
+	});
+	//add counter
+	function updateCount(){
+		var items = $('li').length;
+		$('#counter').text(items);
+	}
+	updateCount();
+	
+	//showing input
+	$newItemButton.show();
+	$newItemForm.hide();
+	$('button').on('click',function(){
+		$newItemButton.hide();
+		$newItemForm.fadeIn(1000);
 	});
 	
 	
