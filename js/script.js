540676590536b1a30acaf52cbbfb6848f1499da4
Tetsumote <unionproject88@gmail.com>
2017-05-10 02:34:14 +0200

    com.5 add jquery animation

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(function(){
+	var $list, $newItemForm, $newItemButton;
+	var item = '';
+	$list = $('ul');
+	$newItemForm = $('#newItemForm');
+	$newItemButton = $('#newItemButton');
+	
+	$('li').hide().each(function(index){
+		$(this).delay(450 * index).fadeIn(1600);
+	});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+});
